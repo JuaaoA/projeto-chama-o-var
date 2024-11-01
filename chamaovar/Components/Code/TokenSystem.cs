8f3ca09d45175044ecced49cda03609da56bbd2a
Juaocareca <joaoaugusto-santana@hotmail.com>
2024-10-31 21:16:12 -0300

    adicao sistema de tokens + ajustes

---
+++ chamaovar/Components/Code/TokenSystem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace chamaovar.Components.Code
+{
+	public static class TokenSystem
+	{
+		public static void InserirNovoToken(string token)
+		{
+			//
+			Token newToken = new Token();
+
+			newToken.nome = token;
+
+			string fileName = "token.json";
+			string jsonString = JsonSerializer.Serialize(newToken);
+			File.WriteAllText(fileName, jsonString);
+		}
+
+		public static async Task<Torcedor?> GetTorcedorByToken()
+		{
+			// Iniciar http
+			HttpClient http = new HttpClient();
+
+			// Pegar o token
+			Token? tk = GetToken();
+
+			// Se o token for nulo
+			if (tk == null)
+			{
+				return null;
+			}
+
+			// Realizar um Get do torcedor
+			var resultado = await http.GetAsync($"https://localhost:7094/chamaovar-api/torcedor/token?nome_token={tk.nome}");
+
+			// Pegar o status code do get
+			int statusCode = (int)resultado.StatusCode;
+
+			// Se o Resultado deu 200, ou seja, deu certo
+			if (statusCode == 200)
+			{
+				// Pegar o torcedor no json indicado
+				Torcedor? tcdr = JsonSerializer.Deserialize<Torcedor>(await resultado.Content.ReadAsStringAsync());
+
+				// Retornar o torcedor
+				return tcdr;
+			}
+
+			// Caso o resultado tenha dado erro, retornar nulo
+			return null;
+		}
+
+		public static Token? GetToken()
+		{
+			// Ler o arquivo de json e armazenar em texto numa variável
+			string jsonToken = File.ReadAllText("token.json");
+
+			// Deserializar o json e transformar em objeto Token
+			Token? tk = JsonSerializer.Deserialize<Token>(jsonToken);
+
+			return tk;
+        }
+	}
+
+	public class Token
+	{
+		public string nome { get; set; }
+	}
+}
+
--- chamaovar/Components/Code/Torcedor.cs
+++ chamaovar/Components/Code/Torcedor.cs
@@ -7,7 +7,6 @@ namespace chamaovar.Components.Code
 		{
 		}
 
-
         public string nome_completo { get; set; }
         public string cpf { get; set; }
         public string email { get; set; }
